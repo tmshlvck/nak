6d2c51a735dbc38f0eb47c6478a1aee4d1858e2f
Tomas Hlavacek <tmshlvck@gmail.com>
2020-07-14 20:30:31 +0000

    Add support for Dell and Cisco full config/deconfig

--- yaml2box.py
+++ yaml2box.py
@@ -6,42 +6,82 @@ import jinja2
 import yaml
 
 
-def applyconf(boxtype, conf, host, user, passwd, secret=None):
-  driver = napalm.get_network_driver(boxtype)
-  if secret:
-    device = driver(host, username=user, password=passwd, optional_args={"global_delay_factor": 3, 'secret': secret})
-  else:
-    device = driver(host, username=user, password=passwd, optional_args={"global_delay_factor": 3})
-    #device = driver(host, user, passwd)
-  device.open()
-  print("============================")
-  print("get_config():")
-  print(str(device.get_config()))
-  print("============================")
-  print("get_facts():")
-  print(str(device.get_facts()))
-  print("============================")
+ignore_vlans = [1,1002, 1003, 1004, 1005]
 
-  print("compare_config():")
+
+def apply_conf(conf, device, simulate=False):
   device.load_merge_candidate(config=conf)
+  print("=== Diff ===")
   print(device.compare_config())
-  print("============================")
-  device.discard_config()
-  #device.commit_config()
+  print("=== End diff ===")
+  if simulate:
+    device.discard_config()
+  else:
+    device.commit_config()
 
-  device.close()
-  print("close() finished")
+
+def get_configured(vlans):
+  confports = set()
+  for v in vlans:
+    if int(v) in ignore_vlans:
+      continue
+    confports |= set(vlans[v]['interfaces'])
+  return confports
+ 
+
+def gen_cleanup(config, vlans):
+  configured_ports = get_configured(vlans)
+
+  config['remove_vlans'] = []
+  for v in [int(k) for k in vlans]:
+    if not v in config['vlans'] and not v in ignore_vlans:
+      config['remove_vlans'].append(v)
+
+  config['clean_ports'] = []
+  for p in config['ports']:
+    if not config['ports'][p].get('descr') and config['ports'][p].get('shutdown') and \
+      p in configured_ports:
+      config['clean_ports'].append(p)
+
+  for p in config['clean_ports']:
+    del(config['ports'][p])
+
+  return config
 
 
-#def confgen(t, yml):
-def confgen(t, fh):
-#  with open(yml, 'r') as fh:
-  config = yaml.safe_load(fh)
-    
+def read_conf(fhs):
+  def merge_conf(res, frag, hostname=None):
+    for swname in frag:
+      if swname == 'all' or not hostname or swname == hostname:
+        for k in frag[swname]:
+          res[k] = frag[swname][k]
+
+  config = {}
+  for fh in fhs:
+    c = yaml.safe_load(fh)
+    merge_conf(config, c)
+
+  return config
+
+
+def merge_vlans(a,b):
+  res = []
+  if type(a) is list:
+    res += a
+  else:
+    res.append(a)
+  if type(b) is list:
+    res += b
+  else:
+    res.append(b)
+  return sorted(res)
+
+
+def gen_text(c, t):
   jenv = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), autoescape=False)
-  # funkce jenv.globals.update(community_fmt=community_fmt)
+  jenv.globals.update(merge_vlans=merge_vlans)
   t = jenv.get_template('%s.j2' % t)
-  return t.render(config=config)
+  return t.render(config=c)
 
 
 
@@ -52,16 +92,30 @@ def confgen(t, fh):
 @click.option('-u', '--user', 'u', help="user")
 @click.option('-p', '--passwd', 'p', help="password/secret")
 @click.option('-e', '--enable', 'enab', help="enable secret")
-@click.argument('yml', type=click.File('r'), nargs=1)
-def main(t, h, u, p, enab, yml):
+@click.option('-s', '--simulate', 'sim', help="simulate", is_flag=True)
+@click.argument('ymls', type=click.File('r'), nargs=-1)
+def main(t, h, u, p, enab, sim, ymls):
   """
   Take YML file, translate it to <type> config and print or  apply it to a box.
   """
-  c = confgen(t, yml)
-  print(c)
-  applyconf(t, c, h, u, p, enab)
+
+  driver = napalm.get_network_driver(t)
+  if enab:
+    device = driver(h, username=u, password=p, optional_args={"global_delay_factor": 3, 'secret': enab})
+  else:
+    device = driver(h, username=u, password=p, optional_args={"global_delay_factor": 3})
+  device.open()
+
+  conf = read_conf(ymls)
+  gen_cleanup(conf, device.get_vlans())
+  tc = gen_text(conf, t)
+  if sim:
+    print("=== Generated config ===")
+    print(tc)
+    print("=== End generated config ===")
+  apply_conf(tc, device, sim)
     
-  
+  device.close()
   
 if __name__ == '__main__':
   main()
